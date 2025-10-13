19883709dd6ad5aaf5d328b2aebb889411ccb987
BoysChell <meisyauserbot@gmail.com>
2025-10-13 22:02:06 +0700

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,8 +3,4 @@ from os.path import basename, dirname, isfile
 
 def LoadPlugins():
     mod_paths = glob(f"{dirname(__file__)}/*.py")
-    return sorted([
-        basename(f)[:-3] 
-        for f in mod_paths 
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ])
+    return sorted([basename(f)[:-3] for f in mod_paths if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")])
