df6cc34173cc9c5b99a9cf2b7336f8da98a496c2
BoysChell <meisyauserbot@gmail.com>
2025-10-13 21:52:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pyrogram.types import Message
 from dotenv import load_dotenv
 import importlib
 import config
-from ChatAiBot.plugins import LoadPlugins
+from plugins import LoadPlugins
 
 load_dotenv()
 
