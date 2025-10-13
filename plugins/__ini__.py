2d8d497ae4e1843cae55236d3d121fe0dbf37ef8
BoysChell <meisyauserbot@gmail.com>
2025-10-13 21:40:14 +0700

    Update __ini__.py

--- plugins/__ini__.py
+++ plugins/__ini__.py
@@ -1,9 +1,10 @@
-from . import ChatAi
-from . import blacklist
-from . import grup
+from glob import glob
+from os.path import basename, dirname, isfile
 
-def register(app):
-    modules = [ChatAi, blacklist, grup]
-    for mod in modules:
-        if hasattr(mod, "register") and callable(mod.register):
-            mod.register(app)
+def LoadPlugins():
+    mod_paths = glob(f"{dirname(__file__)}/*.py")
+    return sorted([
+        basename(f)[:-3] 
+        for f in mod_paths 
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ])
