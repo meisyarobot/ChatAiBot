ba689a6344b6474f562fe635381583554f52693a
BoysChell <meisyauserbot@gmail.com>
2025-10-13 13:58:53 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,14 @@
 
 clear
 
+echo "📦 Menyiapkan virtual environment untuk CHAT AI BOT"
+echo "CREATE BY: @BoysChell"
+
 pip3 install -r req*
 rm -rf *session*
 
+clear
 python3 Chell.py
+
+echo "BOT AI CHAT SUDAH BISA DI GUNAKAN"
+echo "CREATE BY: @BoysChell"
